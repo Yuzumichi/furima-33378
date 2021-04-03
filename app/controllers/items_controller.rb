ee01d8fd6c08b16b2ae5ba6b272fce61beac76b5
Yuzumichi <yuzukaze0112@gmail.com>
2021-04-03 18:08:26 +0900

    テーブル作成とActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,20 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :explanation, :status_id, :category_id, :delivery_charge_id, :delivery_area_id, :delivery_day_id, :image).merge(user_id: current_user.id)
+  end
+
 end
