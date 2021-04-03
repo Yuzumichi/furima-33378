2281977c26a82308caf2b4244c1549516496c743
Yuzumichi <yuzukaze0112@gmail.com>
2021-04-03 18:53:45 +0900

    ActiveHash導入とバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,18 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :delivery_area
+  belongs_to :delivery_day
+  end
+
+  with_potions presence: true do
+    validates :name, :price, :explanation, :image
+    with_options numericality: { other_than: 1 } do
+      validates :status_id, :category_id, :delivery_charge_id, :delivery_area_id, :delivery_day_id
+    end
+  end
 end
