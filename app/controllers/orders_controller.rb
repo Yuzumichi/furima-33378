6e93cf320c06b84de41e22ebbcea76ab01c02ccb
Yuzumichi <yuzukaze0112@gmail.com>
2021-04-18 18:15:25 +0900

    Formオブジェクトの作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,23 @@
+class OrdersController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+    @user_order = UserOrder.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @user_order = UserOrder.new(order_params)
+    if @user_order.valid?
+      @user_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:user_order).permit(:prefecture_id, :post_code, :area_city, :area2_address, :area3_building, :phone_number)
+  end
+end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,26 +1,20 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :user, :item, :prefecture_id, :post_code, :area_city, :area2_address, :area3_building, :phone_number, :buyer
+  attr_accessor :user_id, :item_id, :prefecture_id, :post_code, :area_city, :area2_address, :area3_building, :phone_number
 
   with_options presence: true do
-    validates :user
-    validates :item
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :area_city
+    validates :prefecture_id, numericality: { other_than: 0, message: "select" }
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Include hyphen(-)" }
+    validates :area_city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Input full-width characters."}
     validates :area2_address
-    validates :phone_number, numericality: { only_integer: true, message: "is invalid. Input half-width characters." }
-    validates :buyer
+    validates :phone_number, numericality: { only_integer: true, message: "Input only number." }
   end
 
+
   def save
-    # ユーザーの情報を保存し、「user」という変数に入れている
-    user = User.create(nickname: nickname, email: email, birthday: birthday, first_name: first_name, last_name: last_name, first_name_kana: first_name_kana, last_name_kana: last_name_kana, password: password)
-    # 商品を保存し、「item」という変数に入れている
-    item = Item.create(name: name, explanation: explanation, image: image, status_id: status_id, category_id: category_id, delivery_charge_id: delivery_charge_id, delivery_area_id: delivery_area_id, delivery_day_id: delivery_day_id, price: price)
     # 購入者の情報を保存
-    buyer = Buyer.create(item_id: item.id, user_id: user.id)
+    buyer = Buyer.create(item_id: item_id, user_id: user_id)
     # 購入地域の情報を保存
-    Address.create(prefecture_id: prefecture_id, post_code: post_code, area_city: area_city, area2_address: area2_address, : area3_building: area3_building, phone_number: phone_number, buyer_id: buyer.id)
+    Address.create(prefecture_id: prefecture_id, post_code: post_code, area_city: area_city, area2_address: area2_address, area3_building: area3_building, phone_number: phone_number, buyer_id: buyer.id)
   end
 end
