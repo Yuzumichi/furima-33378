e1a35d583d5169395bf4707ba3807f416cabbe51
Yuzumichi <yuzukaze0112@gmail.com>
2021-04-24 20:31:44 +0900

    購入機能の修正④

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_root
-    unless @item.user_id == current_user.id || @item.buyer.nil?
+    if @item.user_id != current_user.id || @item.buyer.present?
       redirect_to root_path
     end
   end
